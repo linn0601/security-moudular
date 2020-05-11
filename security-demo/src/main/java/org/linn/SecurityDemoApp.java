ce42323c161ddf823465258c7da7fd324cb26e9d
linn <linn0601@qq.com>
2020-05-11 09:40:38 +0800

    1.项目基本模块的搭建和验证成功

---
+++ security-demo/src/main/java/org/linn/SecurityDemoApp.java
@@ -0,0 +1,11 @@
+package org.linn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDemoApp.class, args);
+    }
+}
